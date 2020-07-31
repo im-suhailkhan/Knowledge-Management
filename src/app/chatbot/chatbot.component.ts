5345fe1c8a3b15a6d45c8c0ece39e286abc00788
suhailkhanqbox <suhail.khan.ftg@gmail.com>
2020-08-01 01:18:45 +0530

    made chnages add bot and user icon and msg issue resolve

--- src/app/chatbot/chatbot.component.ts
+++ src/app/chatbot/chatbot.component.ts
@@ -10,79 +10,66 @@ import { ChatbotService } from '../chatbot.service';
 
 export class ChatbotComponent implements OnInit {
 
-  // @HostListener('click') onClick(){
-
-  //   var div=this.render.createElement('div');
-  //   var text=this.render.createText('hrllo');
-  //   this.render.appendChild(div,text);
-  //   //this.render.addClass(div,"msj macro");
-  //   this.render.appendChild(this.el.nativeElement,div);
-  // }
   
   @ViewChild('cgi') div:ElementRef;
+  @ViewChild('cgi1') div1:ElementRef;
   constructor(private botservice:ChatbotService,private render:Renderer2,private el:ElementRef) { }
 
   ngOnInit() {
     
   }
-
+   z:string=0;
   reply:string;
   inputtext:string;
   useravatar:String="https://image.flaticon.com/icons/png/512/306/306473.png";
   botavatar:String="https://getdrawings.com/free-icon/bot-icon-52.png";
-  flag1:boolean=false;
-  flag2:boolean=false;
-  appendbothtml:string='<li style=width:"100%;"><div class="msj-rta macro"><div class="text text-r"><p>{{this.reply}}</p><p><small></small></p></div><div class="avatar" style="padding:0px 0px 0px 10px !important"><img class="img-circle" style="width:100%;" src="{{botavatar}}" /></div></div></li>';
-  appenduserhtml:string='<li style="width:100%"> <div class="msj macro"><div class="avatar"><img class="img-circle" style="width:100%;" src="{{useravatar}}"/></div> <div class="text text-l"><p> {{inputtext}} </p><p><small>  </small></p></div></div></li>';
-  appenduserhtml1:string=this.inputtext;
-  appendbothtml2:string=this.reply;
-  myhtml:string='';
-  // x1:string='<li style=width:"100%;">';
-  // x2:string='<div class="msj macro" style="max-width: 300px;padding: 10px;background-color: white;">';
-  // x3:string='<div class="avatar" style="display:flex;justify-content:center; align-items:center;width:25%;float:left;padding-right:10px;">';
-  // x7:string='<img class="img-circle" style="width:100%;" src="https://image.flaticon.com/icons/png/512/306/306473.png"/>';
-  // x5:string='</div><div class="text text-l">';
-  // x6:string='</div></div></li>';
-  // x8:string='<div class="msj-rta macro">'
-  // x9:string='<div class="text text-r">';
-  // x10:string='</div><div class="avatar" style="padding:0px 0px 0px 10px !important">';
-  // x11:string='<img class="img-circle" style="width:100%;" src="https://image.flaticon.com/icons/png/512/306/306473.png"/>';
- 
+  flag1:boolean=true;
+  flag2:boolean=true;
+  
   insertUserChat():void
 {
-      //this.flag1=true;
       console.log(this.inputtext);
-      var d1=this.el.nativeElement.querySelector('ul')
-      d1.insertAdjacentHTML('beforeend','<div style="margin-top:5px;width:85%;border-radius:5px;padding:5px;display:flex;float:left;background:#d0efff;">');
-      d1.insertAdjacentHTML('beforeend',this.inputtext);
-      
-      // var d2=this.el.nativeElement.querySelector('msj macro')
-      // d2.insertAdjacentHTML('beforeend','<div class="avatar"></div>');
-      // var d4=this.el.nativeElement.querySelector('avatar')
-      // d4.insertAdjacentHTML('beforeend','<img class="img-circle" style="width:100%;" src="{{useravatar}}"></div>');
-      // var d3=this.el.nativeElement.querySelector('msj macro')
-      // d3.insertAdjacentHTML('beforeend','<div class="text text-l"></div>');
-      // var div=this.render.createElement('div');
-      // var text=this.render.createText(this.inputtext);
-      //this.render.appendChild(div,text);
-      //this.render.addClass(div,'chat-popup');
-      // const element=this.render.selectRootElement('.to');
-      // this.render.appendChild(element,text);
-     //this.myhtml= this.myhtml + this.x1+ this.x2+this.x5+ this.inputtext+this.x6+this.x7;
+      this.z=this.z+1;
+      console.log(this.z);
+      const p:HTMLParagraphElement=this.render.createElement('p');
+      p.innerHTML=this.inputtext;
+      p.style.wordWrap="break-word";
+      this.render.appendChild(this.div.nativeElement,p);
+      var img2 = document.createElement('img'); // Use DOM HTMLImageElement
+      img2.src = "https://image.flaticon.com/icons/png/512/306/306473.png";
+      img2.style.width = "50px";
+      img2.style.height = "50px";
+      img2.style.marginTop="-60px";
+      img2.style.marginLeft="-10px";
+      this.render.addClass(this.div.nativeElement, 'hp');
+      this.render.appendChild(this.div.nativeElement,img2);
+      //const i:HTMLPictureElement=this.render.createElement('img');
+      // p.innerHTML=this.inputtext;
+      // this.render.appendChild(this.div.nativeElement,i);
       
       this.botservice.getReply(this.inputtext).subscribe((res:any)=>{
         this.reply=res;
-        this.flag2=true;
+        
       })
-      //this.insertBotChat();
+      this.insertBotChat();
 }
 
 insertBotChat():void
 {
-      //this.flag1=true;
-      console.log(this.reply);
-      this.myhtml= this.myhtml + this.x1+this.x8+this.x9+'what'+this.x10+this.x11+this.x6;
       
+      console.log(this.reply);
+      const p:HTMLParagraphElement=this.render.createElement('p');
+      p.innerHTML="bot reply";
+      p.style.wordWrap="break-word";
+      //p.style.position="absolute";
+      this.render.appendChild(this.div.nativeElement,p);
+      var img3 = document.createElement('img'); // Use DOM HTMLImageElement
+      img3.src = "https://getdrawings.com/free-icon/bot-icon-52.png";
+      img3.style.width = "50px";
+      img3.style.height = "50px";
+      img3.style.marginTop="-70px";
+      img3.style.marginLeft="-10px";
+      this.render.appendChild(this.div.nativeElement,img3);
 }
 
 openForm() {
